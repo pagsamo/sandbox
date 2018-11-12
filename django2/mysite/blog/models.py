20c5c0d315cc079901e24e59474630dc9b09c376
Christian Mark Bituin <bituin.chrismark@gmail.com>
2018-11-12 12:33:15 +0800

    problem on share email

--- django2/mysite/blog/models.py
+++ django2/mysite/blog/models.py
@@ -8,6 +8,7 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
 
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
